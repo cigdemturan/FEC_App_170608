e999fad91d896abce6494e09c9e9670fc18b92dc
CBS-Guest <CBS-Guest@CBS-VL-0003>
2017-07-01 18:56:06 +0800

    Add project files.

---
+++ FEC_App_170608/Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Kinect;
+using Microsoft.Kinect.Face;
+using System.IO;
+
+namespace FEC_App_170608
+{
+    class Program
+    {
+        public static KinectSensor _sensor = null;
+        public static MultiSourceFrameReader _multiSourceFrameReader = null;
+        public static HighDefinitionFaceFrameSource _faceSource = null;
+        public static HighDefinitionFaceFrameReader _faceReader = null;
+        public static FaceAlignment _faceAlignment = null;
+        public static FaceModel _faceModel = null;
+        public static CoordinateMapper _coordinateMapper = null;
+        public static Worker _worker = new Worker();
+        public static FileStream _animationUnitStream, _vertices2DFileStream, _vertices3DFileStream;
+        public static bool trackingSuccess = false;
+        static void Main(string[] args)
+        {
+            _sensor = KinectSensor.GetDefault();
+
+            if (_sensor != null)
+            {
+                _sensor.Open();
+                Console.WriteLine("sensorOpened");
+                if (_sensor.IsOpen)
+                {
+                    _coordinateMapper = _sensor.CoordinateMapper;
+                    _multiSourceFrameReader = _sensor.OpenMultiSourceFrameReader(FrameSourceTypes.Color |FrameSourceTypes.Depth | FrameSourceTypes.Body | FrameSourceTypes.BodyIndex);
+                    _multiSourceFrameReader.MultiSourceFrameArrived += multiSourceReader_FrameArrived;
+
+                    _faceSource = new HighDefinitionFaceFrameSource(_sensor);
+                    _faceReader = _faceSource.OpenReader();
+                    _faceReader.FrameArrived += FaceReader_FrameArrived;
+
+                    _faceModel = new FaceModel();
+                    _faceAlignment = new FaceAlignment();
+
+                }
+            }
+            string input = Console.ReadLine();
+            _sensor.Close();
+        }
+
+        static void multiSourceReader_FrameArrived(object sender, MultiSourceFrameArrivedEventArgs e)
+        {
+
+            MultiSourceFrame _multiSourceFrame = e.FrameReference.AcquireFrame();
+            if (_multiSourceFrame == null)
+            {
+                return;
+            }
+            if (_worker.counterFile == 0 && _worker.counterFrame == 0)
+            {
+                _worker.CreateFolders();
+                _worker.CreateFolderRGB();
+                _animationUnitStream = _worker.InilizeAnimationUnitStream();
+                _vertices2DFileStream = _worker.InilizeVertices2DStream();
+                _vertices3DFileStream = _worker.InilizeVertices3DStream();
+            }
+            //Console.WriteLine(_worker.counterFrame);
+            //long milliseconds = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+            TimeSpan timeSpan = DateTime.Now.TimeOfDay;
+            string timeNow = timeSpan.ToString();
+            timeNow = timeNow.Replace('.', '_');
+            timeNow = timeNow.Replace(':', '_');
+            //Console.WriteLine(timeNow);
+
+            Task.Factory.StartNew(() =>
+            {
+                ColorFrame _colorFrame = null;
+                using (_colorFrame = _multiSourceFrame.ColorFrameReference.AcquireFrame())
+                {
+                    if (_colorFrame != null)
+                    {
+                        _worker.WriteColor(_colorFrame, timeNow);
+                    }
+                }
+            });
+
+            /*
+            Task.Factory.StartNew(() =>
+            {
+                DepthFrame _depthFrame = null;
+                using (_depthFrame = _multiSourceFrame.DepthFrameReference.AcquireFrame())
+                {
+                    //_worker.WriteDepth(_depthFrame, milliseconds);
+                    if (_depthFrame != null)
+                    {
+                        _worker.WriteDepthInColorSpace(_depthFrame, _coordinateMapper, milliseconds);
+                    }
+                    
+                }
+            });
+            */
+
+            Task.Factory.StartNew(() =>
+            {
+                BodyFrame _bodyFrame = null;
+                using (_bodyFrame = _multiSourceFrame.BodyFrameReference.AcquireFrame())
+                {
+                    if (_bodyFrame != null)
+                    {
+                        _bodyFrame.GetAndRefreshBodyData(_worker._bodies);
+                        //Console.WriteLine("bodyArrived");
+
+                        Body body = _worker._bodies.Where(b => b.IsTracked).FirstOrDefault();
+
+                        if (!_faceSource.IsTrackingIdValid)
+                        {
+                            if (body != null)
+                            {
+                                _faceSource.TrackingId = body.TrackingId;
+                            }
+                        }
+                    }
+                }
+            });
+
+            if (_worker.counterFrame++ >= 1000)
+            {
+                //Console.WriteLine("here");
+                Console.WriteLine("999 frames have been saved..");
+                _worker.counterFrame = 0;
+                _worker.counterFile++;
+                _worker.CreateFolderRGB();
+                _animationUnitStream = _worker.InilizeAnimationUnitStream();
+                _vertices2DFileStream = _worker.InilizeVertices2DStream();
+                _vertices3DFileStream = _worker.InilizeVertices3DStream();
+            }
+
+
+        }
+
+        static void FaceReader_FrameArrived(object sender, HighDefinitionFaceFrameArrivedEventArgs e)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                TimeSpan milliseconds = DateTime.Now.TimeOfDay;
+                //Console.WriteLine(milliseconds);
+                using (var _faceFrame = e.FrameReference.AcquireFrame())
+                {
+
+                    if (_faceFrame != null && _faceFrame.IsFaceTracked)
+                    {
+                        if (!trackingSuccess)
+                        {
+                            Console.WriteLine("started face tracking..");
+                            trackingSuccess = true;
+                        }
+                        //Console.WriteLine("faceArrived");
+                        _faceFrame.GetAndRefreshFaceAlignmentResult(_faceAlignment);
+                        Task.Factory.StartNew(() =>
+                        {
+                            _worker.WriteAnimationUnits(_faceAlignment, _animationUnitStream, milliseconds);
+                        });
+                        Task.Factory.StartNew(() =>
+                        {
+                            _worker.WriteVertices(_faceModel, _faceAlignment, _vertices3DFileStream, _vertices2DFileStream, _coordinateMapper, milliseconds);
+                        });
+                    }
+                    else
+                    {
+                        if (trackingSuccess)
+                        {
+                            Console.WriteLine("tracking is lost..");
+                            trackingSuccess = false;
+                        }
+                        _worker.WriteAnimationUnits(_faceAlignment, _animationUnitStream, milliseconds, false);
+                        _worker.WriteVertices(null, null, _vertices3DFileStream, _vertices2DFileStream, null, milliseconds, false);
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ FEC_App_170608/Worker.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Kinect;
+using Microsoft.Kinect.Face;
+using System.Xml;
+using System.IO;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.Windows;
+using System.Drawing;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows;
+using System.Runtime.InteropServices;
+
+namespace FEC_App_170608
+{
+    class Worker
+    {
+        public string _fileName { get; set; }
+        private string _folderNameRec = "recording";
+        private string _rgbFolder, _depthFolder, _verticesFolder, _animationUnitFolder;
+        //public FileStream jointsFileStream { get; set; }
+        public int counterFile;
+        public int counterFrame;
+        private readonly int bytesPerPixel = (PixelFormats.Bgr32.BitsPerPixel + 7) / 8;
+        private readonly int bytesPerPixelBI = (PixelFormats.Gray8.BitsPerPixel + 7) / 8;
+        public FrameDescription colorFrameDescription { get; set; }
+        public Body[] _bodies = new Body[6];
+
+        public void CreateFolders()
+        {
+            //_rgbFolder = _folderNameRec + "/rgb/";
+            //Directory.CreateDirectory(_rgbFolder);
+            //_depthFolder = _folderNameRec + "/depth/";
+            //Directory.CreateDirectory(_depthFolder);
+            _verticesFolder = _folderNameRec + "/vertices/";
+            Directory.CreateDirectory(_verticesFolder);
+            _animationUnitFolder = _folderNameRec + "/AnU/";
+            Directory.CreateDirectory(_animationUnitFolder);
+        }
+        public void CreateFolderRGB()
+        {
+            _rgbFolder = _folderNameRec + "/rgb_" + counterFile + "/";
+            Directory.CreateDirectory(_rgbFolder);
+        }
+        public FileStream InilizeVertices3DStream()
+        {
+            //long milliseconds = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+            //string verticesFile = _verticesFolder + "vertices_" + counterFile + "_" + milliseconds + ".txt";
+            string verticesFile = _verticesFolder + "vertices3D_" + counterFile + ".txt";
+            FileStream verticesFileStream = new FileStream(verticesFile, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            return verticesFileStream;
+        }
+        public FileStream InilizeVertices2DStream()
+        {
+            //long milliseconds = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+            //string verticesFile = _verticesFolder + "vertices_" + counterFile + "_" + milliseconds + ".txt";
+            string verticesFile = _verticesFolder + "vertices2D_" + counterFile + ".txt";
+            FileStream verticesFileStream = new FileStream(verticesFile, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            return verticesFileStream;
+        }
+        public FileStream InilizeAnimationUnitStream()
+        {
+            //long milliseconds = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+            //string animationUnitFile = _animationUnitFolder + "AnU_" + counterFile + "_" + milliseconds + "_.txt";
+            string animationUnitFile = _animationUnitFolder + "AnU_" + counterFile + ".txt";
+            FileStream animationUnitStream = new FileStream(animationUnitFile, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            return animationUnitStream;
+        }
+
+        public void WriteColor(ColorFrame _colorFrame, string milliseconds)
+        {
+            string filePath = _rgbFolder + "/image_" + milliseconds + ".png";
+            colorFrameDescription = _colorFrame.FrameDescription;
+            //Console.WriteLine("colorFrame is here");
+            byte[] pixelsColor = new byte[colorFrameDescription.Width * colorFrameDescription.Height * bytesPerPixel];
+            if (_colorFrame.RawColorImageFormat == ColorImageFormat.Bgra)
+            {
+                _colorFrame.CopyRawFrameDataToArray(pixelsColor);
+            }
+            else
+            {
+                _colorFrame.CopyConvertedFrameDataToArray(pixelsColor, ColorImageFormat.Bgra);
+            }
+
+            int stride = colorFrameDescription.Width * PixelFormats.Bgr32.BitsPerPixel / 8;
+            BitmapSource _imageRGB = BitmapSource.Create(colorFrameDescription.Width, colorFrameDescription.Height, 96, 96, PixelFormats.Bgr32, null, pixelsColor, stride);
+            BitmapSource _imageRGB_cropped = new CroppedBitmap(_imageRGB, new Int32Rect(850, 250, 400, 500));
+            using (FileStream rgb_FileStream = new FileStream(filePath, FileMode.Create))
+            {
+                BitmapEncoder encoder = new PngBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(_imageRGB_cropped));
+                encoder.Save(rgb_FileStream);
+                rgb_FileStream.Close();
+                rgb_FileStream.Dispose();
+            }
+
+        }
+
+        public void WriteDepth(DepthFrame _depthFrame, long milliseconds)
+        {
+            FrameDescription depthFrameDescription = _depthFrame.FrameDescription;
+            //uint depthSize = depthFrameDescription.LengthInPixels;
+            byte[] pixelsDepth = null;
+            ushort[] depthData = new ushort[depthFrameDescription.Width * depthFrameDescription.Height]; //Depth
+            pixelsDepth = new byte[depthFrameDescription.Width * depthFrameDescription.Height * bytesPerPixel]; //Depth
+            _depthFrame.CopyFrameDataToArray(depthData);
+
+            ushort minDepth = _depthFrame.DepthMinReliableDistance;
+            ushort maxDepth = _depthFrame.DepthMaxReliableDistance;
+
+            int colorImageIndex = 0;
+            for (int depthIndex = 0; depthIndex < depthData.Length; ++depthIndex)
+            {
+                ushort depth = depthData[depthIndex];
+                byte intensity = (byte)(depth >= minDepth && depth <= maxDepth ? depth : 0);
+
+                pixelsDepth[colorImageIndex++] = intensity; // Blue
+                pixelsDepth[colorImageIndex++] = intensity; // Green
+                pixelsDepth[colorImageIndex++] = intensity; // Red
+
+                ++colorImageIndex;
+            }
+
+            int stride = depthFrameDescription.Width * PixelFormats.Bgr32.BitsPerPixel / 8;
+
+            BitmapSource _imageDepth = BitmapSource.Create(depthFrameDescription.Width, depthFrameDescription.Height, 96, 96, PixelFormats.Bgr32, null, pixelsDepth, stride);
+            //Console.WriteLine("here");
+            string filePath = _depthFolder + "/image" + milliseconds + ".png";
+
+            using (FileStream depth_FileStream = new FileStream(filePath, FileMode.Create))
+            {
+                BitmapEncoder encoder = new PngBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(_imageDepth));
+                encoder.Save(depth_FileStream);
+                //Console.WriteLine("saved..");
+                depth_FileStream.Close();
+                depth_FileStream.Dispose();
+            }
+            _depthFrame.Dispose();
+        }
+
+        public void WriteDepthInColorSpace(DepthFrame _depthFrame, CoordinateMapper _coordinateMapper, long milliseconds)
+        {
+            FrameDescription depthFrameDescription = _depthFrame.FrameDescription;
+            byte[] pixelsDepth = null;
+            ushort[] depthData = new ushort[depthFrameDescription.Width * depthFrameDescription.Height]; //Depth
+            pixelsDepth = new byte[colorFrameDescription.Width * colorFrameDescription.Height * bytesPerPixel]; //Depth
+            
+            _depthFrame.CopyFrameDataToArray(depthData);
+            DepthSpacePoint[] _depthSpacePoints = new DepthSpacePoint[colorFrameDescription.Width * colorFrameDescription.Height];
+            _coordinateMapper.MapColorFrameToDepthSpace(depthData, _depthSpacePoints);
+
+            ushort[] depthDataInColor = new ushort[colorFrameDescription.Width * colorFrameDescription.Height];
+
+            // Get the min and max reliable depth for the current frame
+            //ushort minDepth = _depthFrame.DepthMinReliableDistance;
+            //ushort maxDepth = _depthFrame.DepthMaxReliableDistance;
+            ushort minDepth = 500;
+            ushort maxDepth = 2500;
+
+            //Console.WriteLine("{0}", minDepth);
+            //Console.WriteLine("{0}", maxDepth);
+
+            for (int colorY = 0; colorY < colorFrameDescription.Height; colorY++)
+            {
+                for (int colorX = 0; colorX < colorFrameDescription.Width; colorX++)
+                {
+                    //const long colorIndex = colorY * colorFrameDescription.Width + colorX;
+                    int colorIndex = ((colorFrameDescription.Width * colorY) + colorX);
+                    int colorIndexPixel = ((colorFrameDescription.Width * colorY) + colorX) * bytesPerPixel;
+                    //int depthX = static_cast<int>
+                    int depthX = (int)Math.Floor(_depthSpacePoints[colorIndex].X + 0.5);
+                    int depthY = (int)Math.Floor(_depthSpacePoints[colorIndex].Y + 0.5);
+
+                    if ((0 <= depthX && (depthX < depthFrameDescription.Width) && (0 <= depthY) && (depthY < depthFrameDescription.Height)))
+                    {
+                        int depthIndex = depthY * depthFrameDescription.Width + depthX;
+                        depthDataInColor[colorIndex] = depthData[depthIndex];
+
+                        ushort depth = depthData[depthIndex];
+                        byte intensity = (byte)(depth >= minDepth && depth <= maxDepth ? depth : 0);
+                        pixelsDepth[colorIndexPixel] = intensity;
+                        pixelsDepth[colorIndexPixel + 1] = intensity;
+                        pixelsDepth[colorIndexPixel + 2] = intensity;
+                    }
+                }
+            }
+
+
+            int stride = colorFrameDescription.Width * PixelFormats.Bgr32.BitsPerPixel / 8;
+            BitmapSource _imageDepth = BitmapSource.Create(colorFrameDescription.Width, colorFrameDescription.Height, 96, 96, PixelFormats.Bgr32, null, pixelsDepth, stride);
+            string filePath = _depthFolder + "/depth" + milliseconds + ".png";
+            BitmapSource _imageDepth_cropped = new CroppedBitmap(_imageDepth, new Int32Rect(900, 280, 500, 500));
+            using (FileStream depth_FileStream = new FileStream(filePath, FileMode.Create))
+            {
+                BitmapEncoder encoder = new PngBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(_imageDepth_cropped));
+                encoder.Save(depth_FileStream);
+                //Console.WriteLine("saved..");
+                depth_FileStream.Close();
+                depth_FileStream.Dispose();
+            }
+  
+            _depthFrame.Dispose();
+        }
+
+        public void WriteAnimationUnits(FaceAlignment _faceAlignment, FileStream _animationUnitStream, TimeSpan milliseconds, bool flag = true)
+        {
+            var _animationUnitWriter = new StreamWriter(_animationUnitStream);
+            _animationUnitWriter.Write("\r\n");
+            _animationUnitWriter.Write("{0}, ", milliseconds);
+            if (flag)
+            {
+                IReadOnlyDictionary<FaceShapeAnimations, float> _animationUnits = _faceAlignment.AnimationUnits;
+                Dictionary<FaceShapeAnimations, float> _animationUnitsDict = new Dictionary<FaceShapeAnimations, float>();
+
+                foreach (FaceShapeAnimations _anuType in _animationUnits.Keys)
+                {
+                    _animationUnitWriter.Write("{0}, ", _animationUnits[_anuType]);
+                }
+
+            }
+            else
+            {
+                _animationUnitWriter.Write("NaN, ");
+            }
+            _animationUnitWriter.Flush();
+
+        }
+
+        public void WriteVertices(FaceModel _faceModel, FaceAlignment _faceAlignment, FileStream _vertices3DFileStream, FileStream _vertices2DFileStream, CoordinateMapper _coordinateMapper, TimeSpan milliseconds, bool flag = true)
+        {
+            var vertices3DWriter = new StreamWriter(_vertices3DFileStream);
+            vertices3DWriter.Write("\r\n");
+            vertices3DWriter.Write("{0}, ", milliseconds);
+            var vertices2DWriter = new StreamWriter(_vertices2DFileStream);
+            vertices2DWriter.Write("\r\n");
+            vertices2DWriter.Write("{0}, ", milliseconds);
+
+            if (flag)
+            {
+                var _vertices3D = _faceModel.CalculateVerticesForAlignment(_faceAlignment);
+
+                //CameraSpacePoint[] _vertices3D = _faceModel.CalculateVerticesForAlignment(_faceAlignment);
+                //ColorSpacePoint[] _vertices2D = null;
+
+                //_coordinateMapper.MapCameraPointsToColorSpace(_vertices3D, _vertices2D);
+
+                for (int index = 0; index < _vertices3D.Count; index++)
+                {
+                    
+                    CameraSpacePoint vertice3D = _vertices3D[index];
+                    //if (vertice3D.)
+                    ColorSpacePoint vertice2D = _coordinateMapper.MapCameraPointToColorSpace(vertice3D);
+
+                    vertices3DWriter.Write("{0}, ", vertice3D.X);
+                    vertices3DWriter.Write("{0}, ", vertice3D.Y);
+                    vertices3DWriter.Write("{0}, ", vertice3D.Z);
+
+                    vertices2DWriter.Write("{0}, ", vertice2D.X);
+                    vertices2DWriter.Write("{0}, ", vertice2D.Y);
+                    
+                }
+            }
+            else
+            {
+                vertices3DWriter.Write("NaN, ");
+                vertices2DWriter.Write("NaN, ");
+            }
+
+            vertices3DWriter.Flush();
+            vertices2DWriter.Flush();
+        } 
+
+    }
+}
